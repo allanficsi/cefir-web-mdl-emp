f3364291d04efac37d1501162239fabd571be323
Diego <cdiego.lima@gmail.com>
2019-04-01 16:02:05 -0300

    Alterações de ação

--- src/main/java/br/com/aptare/cefit/controller/AcaoController.java
+++ src/main/java/br/com/aptare/cefit/controller/AcaoController.java
@@ -1,5 +1,8 @@
 package br.com.aptare.cefit.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.aptare.cefit.acao.dto.AcaoDTO;
+import br.com.aptare.cefit.acao.dto.AgendaDTO;
 import br.com.aptare.cefit.acao.entity.Acao;
 import br.com.aptare.cefit.acao.service.AcaoService;
 import br.com.aptare.fda.exception.AptareException;
@@ -34,8 +38,8 @@ public class AcaoController extends AptareCrudController<Acao, AcaoService>
    protected void completarInserir(Acao entity, HttpServletRequest request)
    {
       // Se espaco, agenda ou profissional nao estiver preenchidos, situacao = PENDENTE else ATIVA
-      if(entity.getCodigoEspaco() == null 
-            || entity.getCodigoEspaco() <= 0
+      if(entity.getCodigoEsp() == null 
+            || entity.getCodigoEsp() <= 0
             || entity.getListaAgenda() == null 
             || entity.getListaAgenda().size() <= 0
             || entity.getListaAcaoProfissional() == null
@@ -58,13 +62,19 @@ public class AcaoController extends AptareCrudController<Acao, AcaoService>
    @Override
    protected String[] juncaoGet()
    {
-      return new String[] { "espaco", "tipoAcao", "listaAcaoProfissional*.profissional*.cadastroUnico" };
+      return new String[] { "espaco", "tipoAcao", "listaAcaoProfissional*.profissional*.cadastroUnico", "listaAgenda*", "auditoria.usuarioInclusao" };
    }
    
    @Override
    protected String[] ordenacaoPesquisar()
    {
       return new String[] { "nome" };
+   }
+   
+   @Override
+   protected String[] ordenacaoGet()
+   {
+      return new String[] { "listaAgenda.dataAgenda" };
    }
 
    @Override
@@ -86,6 +96,27 @@ public class AcaoController extends AptareCrudController<Acao, AcaoService>
       AcaoDTO dto = new AcaoDTO();
       modelMapper.getConfiguration().setAmbiguityIgnored(true);
       modelMapper.map(acao, dto);
+      
+      //Ordenando listaAgenda
+      if(dto != null 
+            && dto.getListaAgenda() != null
+            && dto.getListaAgenda().size() > 0) 
+      {
+         List<AgendaDTO> listaAgenda = new ArrayList<AgendaDTO>(dto.getListaAgenda());
+         
+         Collections.sort(listaAgenda, new Comparator<AgendaDTO>()
+         {
+            @Override
+            
+            public int compare(AgendaDTO a1, AgendaDTO a2)
+            {
+
+               return a1.getDataAgenda().compareTo(a2.getDataAgenda());
+            }
+         });
+         
+         dto.setListaAgendaOrdenada(listaAgenda);
+      }
       
       return dto;
    }
