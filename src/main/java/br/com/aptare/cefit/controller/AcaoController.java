4a6164c5566a7c202d999947fd24cd8285062894
Diego <cdiego.lima@gmail.com>
2019-04-09 09:04:45 -0300

    Tela de acao

--- src/main/java/br/com/aptare/cefit/controller/AcaoController.java
+++ src/main/java/br/com/aptare/cefit/controller/AcaoController.java
@@ -8,7 +8,10 @@ import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +19,8 @@ import br.com.aptare.cefit.acao.dto.AcaoDTO;
 import br.com.aptare.cefit.acao.dto.AgendaDTO;
 import br.com.aptare.cefit.acao.entity.Acao;
 import br.com.aptare.cefit.acao.service.AcaoService;
+import br.com.aptare.cefit.response.Response;
+import br.com.aptare.cefit.util.RetirarLazy;
 import br.com.aptare.fda.exception.AptareException;
 
 @RestController
@@ -28,10 +33,21 @@ public class AcaoController extends AptareCrudController<Acao, AcaoService>
       super(AcaoService.getInstancia());
    }
    
-   @Override
-   protected void ativarInativar(Acao entity) throws AptareException
+   @PostMapping(path = "/alterarSituacaoAcao")
+   public ResponseEntity<Response<Object>> alterarSituacaoAcao(HttpServletRequest request, @RequestBody Acao acao)
    {
-      this.getService().ativarInativar(entity);
+      Response<Object> response = new Response<Object>();
+      try
+      {
+         Acao objAtualizar = new RetirarLazy<Acao>(getService().alterarSituacaoAcao(acao)).execute();
+         response.setData(this.atualizarEntidadeResponse(objAtualizar));
+      }
+      catch (AptareException e)
+      {
+         response.getErrors().add(e.getMensagem());
+         return ResponseEntity.badRequest().body(response);
+      }
+      return ResponseEntity.ok(response);
    }
    
    @Override
@@ -52,6 +68,21 @@ public class AcaoController extends AptareCrudController<Acao, AcaoService>
          entity.setSituacao(AcaoService.ACAO_ATIVA);
       }
    }
+   
+   @Override
+   protected void completarAlterar(Acao entity, HttpServletRequest request)
+   {
+      // Se espaco, agenda ou profissional nao estiver preenchidos, situacao = PENDENTE continua a mesma situacao anterior
+      if(entity.getCodigoEsp() == null 
+            || entity.getCodigoEsp() <= 0
+            || entity.getListaAgenda() == null 
+            || entity.getListaAgenda().size() <= 0
+            || entity.getListaAcaoProfissional() == null
+            || entity.getListaAcaoProfissional().size() <= 0)
+      {
+         entity.setSituacao(AcaoService.ACAO_PENDENTE);
+      }
+   }
 
    @Override
    protected String[] juncaoPesquisar()
@@ -62,13 +93,13 @@ public class AcaoController extends AptareCrudController<Acao, AcaoService>
    @Override
    protected String[] juncaoGet()
    {
-      return new String[] { "espaco", "tipoAcao", "listaAcaoProfissional*.profissional*.cadastroUnico", "listaAgenda*", "auditoria.usuarioInclusao" };
+      return new String[] { "espaco", "tipoAcao", "listaAcaoProfissional*.profissional*.cadastroUnico*", "listaAgenda*", "auditoria.usuarioInclusao" };
    }
    
    @Override
    protected String[] ordenacaoPesquisar()
    {
-      return new String[] { "nome" };
+      return new String[] { "auditoria.dataInclusao*", "nome" };
    }
    
    @Override
@@ -110,7 +141,6 @@ public class AcaoController extends AptareCrudController<Acao, AcaoService>
             
             public int compare(AgendaDTO a1, AgendaDTO a2)
             {
-
                return a1.getDataAgenda().compareTo(a2.getDataAgenda());
             }
          });
